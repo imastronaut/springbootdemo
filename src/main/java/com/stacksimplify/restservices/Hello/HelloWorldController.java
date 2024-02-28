fe0c20a33f9041c2f18201df618f036a16f022fd
kollabhargavre1 <kollabhargavreddy@gmail.com>
2024-02-29 00:23:51 +0530

    i18n - springboot-internalization

--- src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.stacksimplify.restservices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	
 	@GetMapping("/helloworld")
 	public String helloWorld() {
@@ -16,4 +25,15 @@ public class HelloWorldController {
 	public UserDetails helloWorldBean() {
 		return new UserDetails("Bhargav","Reddy", "Madanapalle");
 	}
+	
+	@GetMapping("/hello-int")
+	public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+	
+	@GetMapping("/hello-int2")
+	public String getMessagesInI18NFormat2() {
+		return messageSource.getMessage("label.hello", null,LocaleContextHolder.getLocale());
+	}
 }
+
